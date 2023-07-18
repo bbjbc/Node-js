16aefec90b7e8d6e2cd17613653407f810cd6f6e
bbjbc <jobc4550@gmail.com>
2023-07-19 00:53:38 +0900

    method별 요청으로 미들웨어 실행 제한

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -13,7 +13,8 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
+  // .post를 하면 POST요청에만 응답함, GET에는 응답 X
   console.log(req.body); // 이렇게만 쓰면 req.는 요청 본문을 parsing하지 않기 때문임(그래서 undefined만 뜸)
   res.redirect("/"); // 이 경로로 리다이렉트함
 });
