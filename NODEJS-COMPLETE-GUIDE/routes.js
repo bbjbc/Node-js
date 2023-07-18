e4723b82dfdb171452ab77180b45c1c1d9cff15e
bbjbc <jobc4550@gmail.com>
2023-07-18 18:35:09 +0900

    npm 스크립트의 이해

--- NODEJS-COMPLETE-GUIDE/routes.js
+++ NODEJS-COMPLETE-GUIDE/routes.js
@@ -48,3 +48,29 @@ module.exports = requestHandler;
 // module.exports.handler = requestHandler;
 // module.exports.someText = "Some hard coded text";
 // 앞에 있는 module을 생략 가능
+
+// // Module Summary
+// 1. How the Web Works
+// Client => Request => Server => Response => Client
+
+// 2. Program Lifecycle & Event Loop
+// - Node.js runs non-blocking JS code and uses an event-driven code for running your logic
+// - A Node program exits as soon as there is no more work to do
+// - Note : The createServer() event never finishes by default
+
+// 3. Asynchronous code
+// - JS code is non-blocking
+// - Use callbacks and events => Order changes!
+
+// 4. Requests & Responses
+// - Parse request data in chunks(Streams & Buffers)
+// - Avoid "double responses!"
+
+// 5. Node.js & Core Modules
+// - Node.js ships with multiple core modules (http, fs, path, ...)
+// - Core modules can be imported into any file to be used there
+// - Import via require('module')
+
+// 6. The Node Module System
+// - Import via require('./path-to-file') for custom files or require('module') for core & third-party modules
+// - Export via module.exports or just exports (for multiple exports)
