cecb794466f608ef9d808407ca53b8a9a8a2b2ee
bbjbc <jobc4550@gmail.com>
2023-07-19 17:43:20 +0900

    404 오류 페이지 추가

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -14,6 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
 // const server = http.createServer(app);
 // server.listen(3000);
