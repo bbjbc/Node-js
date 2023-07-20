77f350c4d749029480547e76e3613da5fb9090f9
bbjbc <jobc4550@gmail.com>
2023-07-20 19:56:05 +0900

    절대경로를 위한 헬퍼 함수 사용

--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -2,10 +2,12 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html")); // join은 마지막에 경로를 출력함. 그리고 여러 세그먼트를 이어 붙여서 이 경로를 구축함
+  res.sendFile(path.join(rootDir, "views", "shop.html")); // join은 마지막에 경로를 출력함. 그리고 여러 세그먼트를 이어 붙여서 이 경로를 구축함
   // __dirname은 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 전역 변수임
   // 현재 routes 폴더이기 때문에 옆 폴더로 이동하기 위해서 ../
 });
