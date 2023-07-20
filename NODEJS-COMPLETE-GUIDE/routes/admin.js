048ab96a99c742ea3a451e113574d2711a4ac97c
bbjbc <jobc4550@gmail.com>
2023-07-20 18:41:46 +0900

    경로 필터링

--- NODEJS-COMPLETE-GUIDE/routes/admin.js
+++ NODEJS-COMPLETE-GUIDE/routes/admin.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+    "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
   // .post를 하면 POST요청에만 응답함, GET에는 응답 X
   console.log(req.body); // 이렇게만 쓰면 req.는 요청 본문을 parsing하지 않기 때문임(그래서 undefined만 뜸)
   res.redirect("/"); // 이 경로로 리다이렉트함
