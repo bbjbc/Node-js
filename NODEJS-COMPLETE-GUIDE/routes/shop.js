b57d5b8da59c7ddb31c0b7b446fa6c93e642e83c
bbjbc <jobc4550@gmail.com>
2023-07-24 18:29:33 +0900

    라우트 등록 및 컨트롤러 수정

--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -2,13 +2,19 @@ const path = require("path");
 
 const express = require("express");
 
-const productsController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", productsController.getProducts);
+router.get("/", shopController.getIndex);
 // join은 마지막에 경로를 출력함. 그리고 여러 세그먼트를 이어 붙여서 이 경로를 구축함
 // __dirname은 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 전역 변수임
 // 현재 routes 폴더이기 때문에 옆 폴더로 이동하기 위해서 ../
+
+router.get("/products", shopController.getProducts);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
