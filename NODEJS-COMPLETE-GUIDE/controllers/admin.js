503ef3ecf94381ef2130c43804a4e6088d7fe31f
bbjbc <jobc4550@gmail.com>
2023-07-24 19:10:38 +0900

    제품 데이터 표시

--- NODEJS-COMPLETE-GUIDE/controllers/admin.js
+++ NODEJS-COMPLETE-GUIDE/controllers/admin.js
@@ -9,10 +9,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const imageUrl = req.body.imageUrl;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect("/");
 };
