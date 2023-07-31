f801f7d14c1aca435dd50a8271c486d9c8bb2457
bbjbc <jobc4550@gmail.com>
2023-07-31 19:52:36 +0900

    sequelize 추가 및 데이터베이스 연결

--- NODEJS-COMPLETE-GUIDE/util/database.js
+++ NODEJS-COMPLETE-GUIDE/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "qudcks26", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "qudcks26",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
