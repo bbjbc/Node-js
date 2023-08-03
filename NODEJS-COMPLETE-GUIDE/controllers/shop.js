c29e09df0d7df7e68ae0fdbc4fb224ed811a577d
bbjbc <jobc4550@gmail.com>
2023-08-03 22:33:05 +0900

    관련 상품 추가 및 장바구니 항목 검색하기

--- NODEJS-COMPLETE-GUIDE/controllers/shop.js
+++ NODEJS-COMPLETE-GUIDE/controllers/shop.js
@@ -86,6 +86,8 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fechedCart;
+  let newQuantity = 1;
+  let product;
   req.user
     .getCart()
     .then((cart) => {
@@ -94,19 +96,19 @@ exports.postCart = (req, res, next) => {
     })
     .then((products) => {
       if (products.length > 0) {
-        let product = products[0];
+        product = products[0];
       }
-      let newQuantity = 1;
-      // if (product) {
-      //   // ...
-      // }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fechedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => console.log(err));
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fechedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect("/cart");
