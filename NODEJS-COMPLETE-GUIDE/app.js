34d2856b251eddec73cdf3090f83e0fdef998a9a
bbjbc <jobc4550@gmail.com>
2023-08-03 18:25:22 +0900

    카트 생성 및 가져오기

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -46,8 +46,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
@@ -60,6 +60,7 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
-    app.listen(3000);
+    return user.createCart();
   })
+  .then((cart) => app.listen(3000))
   .catch((err) => console.log(err));
