a4d6f79a3d2c765942d546a4216c9d93ced1ff06
bbjbc <jobc4550@gmail.com>
2023-08-06 16:59:40 +0900

    mongoDB 설치

--- NODEJS-COMPLETE-GUIDE/util/database.js
+++ NODEJS-COMPLETE-GUIDE/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "qudcks26", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://aoo4550:9FDtsrk2VU9h9IAp@cluster0.u8voidr.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    }); // MongoDB에 연결 생성
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
