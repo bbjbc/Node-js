9043b4f21b57b33423c45f33f406deba9921ebde
bbjbc <jobc4550@gmail.com>
2023-08-07 22:01:57 +0900

    단일 제품 가져오기

--- NODEJS-COMPLETE-GUIDE/controllers/shop.js
+++ NODEJS-COMPLETE-GUIDE/controllers/shop.js
@@ -16,6 +16,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
   // Product.findAll({ where: { id: prodId } })
   //   .then((product) => {
   //     res.render("shop/product-detail", {
@@ -25,15 +34,6 @@ exports.getProduct = (req, res, next) => {
   //     });
   //   })
   //   .catch((err) => console.log(err));
-  Product.findByPk(prodId)
-    .then((product) => {
-      res.render("shop/product-detail", {
-        product: product,
-        pageTitle: product.title,
-        path: "/products",
-      });
-    })
-    .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
