fdd14374ccbfba17069743d9725dfaf2c04bc6d2
bbjbc <jobc4550@gmail.com>
2023-08-10 21:34:50 +0900

    cart 항목 및 주문 작업

--- NODEJS-COMPLETE-GUIDE/models/user.js
+++ NODEJS-COMPLETE-GUIDE/models/user.js
@@ -2,14 +2,30 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, _id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = _id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
