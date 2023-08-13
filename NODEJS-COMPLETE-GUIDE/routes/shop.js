dab3fac6661124ceeb5c3ef543ab5dd543564a8b
bbjbc <jobc4550@gmail.com>
2023-08-13 21:43:58 +0900

    모든 제품 가져오기

--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -6,12 +6,12 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-// router.get("/", shopController.getIndex);
+router.get("/", shopController.getIndex);
 // join은 마지막에 경로를 출력함. 그리고 여러 세그먼트를 이어 붙여서 이 경로를 구축함
 // __dirname은 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 전역 변수임
 // 현재 routes 폴더이기 때문에 옆 폴더로 이동하기 위해서 ../
 
-// router.get("/products", shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
 // router.get("/products/:productId", shopController.getProduct);
 
