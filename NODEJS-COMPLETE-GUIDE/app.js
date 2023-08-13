4f732cf111d2e48823a262e354d792d0e2c06c6d
bbjbc <jobc4550@gmail.com>
2023-08-13 20:39:03 +0900

    mongoose설치 및 MongoDB 서버에 연결

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -35,9 +35,14 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://aoo4550:9FDtsrk2VU9h9IAp@cluster0.u8voidr.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => app.listen(3000))
+  .catch((err) => {
+    console.log(err);
+  });
 
 // const sequelize = require("./util/database");
 // const Product = require("./models/product");
--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -6,24 +6,24 @@ const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", shopController.getIndex);
+// router.get("/", shopController.getIndex);
 // join은 마지막에 경로를 출력함. 그리고 여러 세그먼트를 이어 붙여서 이 경로를 구축함
 // __dirname은 운영체제의 절대 경로를 이 프로젝트 폴더로 고정해주는 전역 변수임
 // 현재 routes 폴더이기 때문에 옆 폴더로 이동하기 위해서 ../
 
-router.get("/products", shopController.getProducts);
+// router.get("/products", shopController.getProducts);
 
-router.get("/products/:productId", shopController.getProduct);
+// router.get("/products/:productId", shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
+// router.get("/cart", shopController.getCart);
 
-router.post("/cart", shopController.postCart);
+// router.post("/cart", shopController.postCart);
 
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+// router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
-router.post("/create-order", shopController.postOrders);
+// router.post("/create-order", shopController.postOrders);
 
-router.get("/orders", shopController.getOrders);
+// router.get("/orders", shopController.getOrders);
 
 // router.get("/checkout", shopController.getCheckout);
 
