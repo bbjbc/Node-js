4f81bbb569ee30bc369c969e2b35faeb0e814e0f
bbjbc <jobc4550@gmail.com>
2023-08-16 17:39:30 +0900

    버그 수정

--- NODEJS-COMPLETE-GUIDE/controllers/auth.js
+++ NODEJS-COMPLETE-GUIDE/controllers/auth.js
@@ -1,10 +1,9 @@
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
-  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: true,
+    isAuthenticated: false,
   });
 };
 
