264e82f06f9b82176d186b09ac003a890937c659
bbjbc <jobc4550@gmail.com>
2023-08-20 21:59:35 +0900

    CSRF 토큰 사용 및 미들웨어를 통한 인증과 토큰 과정 추가

--- NODEJS-COMPLETE-GUIDE/controllers/shop.js
+++ NODEJS-COMPLETE-GUIDE/controllers/shop.js
@@ -119,7 +119,6 @@ exports.postOrders = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
           email: req.user.email,
           userId: req.user,
         },
