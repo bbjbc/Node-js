a48922c69c62869b020155d669ece6e5911eaa58
bbjbc <jobc4550@gmail.com>
2023-09-07 19:07:13 +0900

    결제 페이지 추가 및 라우트 연결

--- NODEJS-COMPLETE-GUIDE/controllers/shop.js
+++ NODEJS-COMPLETE-GUIDE/controllers/shop.js
@@ -161,6 +161,29 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
 exports.postOrders = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
@@ -267,10 +290,3 @@ exports.getInvoice = (req, res, next) => {
     })
     .catch((err) => next(err));
 };
-
-// exports.getCheckout = (req, res, next) => {
-//   res.render("shop/checkout", {
-//     path: "/checkout",
-//     pageTitle: "Checkout",
-//   });
-// };
