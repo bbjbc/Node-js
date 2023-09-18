1a13f38617e49886af16b0d299125923908a7877
bbjbc <jobc4550@gmail.com>
2023-09-18 17:52:27 +0900

    사용자 권한 확인 로직 추가

--- REST-API/controllers/feed.js
+++ REST-API/controllers/feed.js
@@ -125,6 +125,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized!");
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -154,6 +159,11 @@ exports.deletePost = (req, res, next) => {
         throw error;
       }
       // Check logged in user
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized!");
+        error.statusCode = 403;
+        throw error;
+      }
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     })
