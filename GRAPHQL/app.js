4ed429cd303902eee4e94ee4a623bf6d41ff2efc
bbjbc <jobc4550@gmail.com>
2023-09-25 19:58:54 +0900

    웹 소켓 삭제로 인한 HTTP CORS 로직 추가 및 OPTION 요청 상태 코드 200으로 전달

--- GRAPHQL/app.js
+++ GRAPHQL/app.js
@@ -38,16 +38,19 @@ app.use(
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(
   "/graphql",
