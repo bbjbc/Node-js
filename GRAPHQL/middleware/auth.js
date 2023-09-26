c08241816cccbf8a4add8430a6c5d864c7835a6e
bbjbc <jobc4550@gmail.com>
2023-09-26 17:32:17 +0900

    auth미들웨어 수정 및 사용자 데이터 추출하기

--- GRAPHQL/middleware/auth.js
+++ GRAPHQL/middleware/auth.js
@@ -3,23 +3,22 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    const error = new Error("Not authenticated.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, "somesupersecretsecret"); // verify가 토큰을 해독할 뿐 아니라 확인하는 과정도 거침
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    req.isAuth = false;
+    return next();
   }
   if (!decodedToken) {
-    const error = new Error("Not authenticated.");
-    error.statusCode = 401;
-    throw error;
+    req.isAuth = false;
+    return next();
   }
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 };
