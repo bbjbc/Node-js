166ccabf7f4d53fbf03d97dad1a8bf0d16f9e7f0
bbjbc <jobc4550@gmail.com>
2023-09-30 00:03:30 +0900

    게시물 삭제 뮤테이션 추가 및 리졸버 적용

---
+++ GRAPHQL/util/file.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fs = require("fs");
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Error deleting file: ", err);
+    } else {
+      console.log("File deleted successfully: ", filePath);
+    }
+  });
+};
+
+exports.clearImage = clearImage;
